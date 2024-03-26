c541b83f51b78dccd7b12eb7002ef8af4324ab4f
meder <meder.tynychbekov@gmail.com>
2024-03-26 23:14:12 +0600

    I changed class Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+
+        System.out.println("your age: " + age);
     }
 }
